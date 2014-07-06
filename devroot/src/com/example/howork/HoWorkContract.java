b943e5296f53c409ff7a3c7c0ba099b212a5c53d
roiko <roiko@hotmail.it>
2014-07-06 13:21:23 +0200

    First code for manage database. Pending event for OUT button added

---
+++ devroot/src/com/example/howork/HoWorkContract.java
@@ -0,0 +1,75 @@
+package com.example.howork;
+
+import android.provider.BaseColumns;
+
+public final class HoWorkContract {
+	/*
+	 * Questa classe serve per gestire il database SQLite
+	 * */
+
+	public HoWorkContract(){}
+	
+	/* One inner class for each table
+	 * Implement BaseColumns allows to manage the automatically 
+	 * created column _ID (as needed by the framework) 
+	 * */
+    public static abstract class DBINFO implements BaseColumns {
+        public static final String TABLE_NAME = "DBINFO";
+        public static final String CN_KEY = "key";
+        public static final String CN_VALUE = "value";
+        
+        //SQL for create the table
+        private static final String SQL_CREATE_DBINFO =
+        	    "CREATE TABLE " + TABLE_NAME + " (" +
+        	    _ID + " INTEGER PRIMARY KEY," +
+        	    CN_KEY + " TEXT," +
+        	    CN_VALUE + " TEXT" + 
+        	    " )";
+      //SQL for drop the table
+        private static final String SQL_DELETE_ENTRIES =
+        	    "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+    
+    public static abstract class DAYS implements BaseColumns {
+        public static final String TABLE_NAME = "DAYS";
+        //public static final String CN_ID = "id";
+        public static final String CN_YEAR = "year";
+        public static final String CN_MONTH = "month";
+        public static final String CN_DAY = "day";
+        
+        private static final String SQL_CREATE_DAYS = 
+        		"CREATE TABLE " + TABLE_NAME + " (" +
+                	    _ID + " INTEGER PRIMARY KEY," +
+                	    CN_YEAR + " INTEGER" +
+                	    CN_MONTH + " INT" +
+                	    CN_DAY + " INT" +
+                	    " )";
+        
+        private static final String SQL_DELETE_DAYS =
+        	    "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+    
+    public static abstract class STAMPS implements BaseColumns {
+        public static final String TABLE_NAME = "STAMPS";
+        public static final String CN_DAY_ID = "day_id";
+        public static final String CN_WAY = "way";
+        public static final String CN_TIME = "time";
+        
+        public static final String WAY_IN = "IN";
+        public static final String WAY_OUT = "OUT";
+        
+        private static final String SQL_CREATE_DAYS = 
+        		"CREATE TABLE " + TABLE_NAME + " (" +
+                	    _ID + " INTEGER PRIMARY KEY," +
+                	    CN_WAY + " TEXT" +
+                	    CN_TIME + " TEXT" +
+                	    CN_DAY_ID + " INTEGER" +
+                	    "CONSTRAINT" +CN_DAY_ID+ "REFERENCES "+ DAYS.TABLE_NAME +"("+ DAYS._ID +") ON DELETE CASCADE)" +
+                	    " )";
+        
+        private static final String SQL_DELETE_STAMPS =
+        	    "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+    
+    //Rocco: riprendi da qui: http://developer.android.com/training/basics/data-storage/databases.html#DbHelper
+}
--- devroot/src/com/example/howork/MyWidgetProvider.java
+++ devroot/src/com/example/howork/MyWidgetProvider.java
@@ -13,14 +13,13 @@ import android.widget.Toast;
 
 public class MyWidgetProvider extends AppWidgetProvider {
 
-	public static String W_IN_CLICKED = "HoWork.BtnWIN";
+	public static String W_IN_CLICKED = "com.roiko.HoWork.BtnWIN";
+	public static String W_OUT_CLICKED = "com.roiko.HoWork.BtnWOUT";
 
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 		Toast.makeText(context, "OnUpdate!", Toast.LENGTH_SHORT).show();
-		
 
-		
 	}
 
 	@Override
@@ -30,13 +29,20 @@ public class MyWidgetProvider extends AppWidgetProvider {
 				R.layout.widget);
 		ComponentName watchWidget = new ComponentName(context,
 				MyWidgetProvider.class);
-		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+		AppWidgetManager appWidgetManager = AppWidgetManager
+				.getInstance(context);
 
 		/* Associo intent a button IN */
 		Intent intent = new Intent(W_IN_CLICKED);
-		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0,
-				intent, PendingIntent.FLAG_UPDATE_CURRENT);
-		rViews.setOnClickPendingIntent(R.id.btnWIN, getPendingSelfIntent(context, W_IN_CLICKED));
+		/*
+		 * PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0,
+		 * intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		 */
+		rViews.setOnClickPendingIntent(R.id.btnWIN,
+				getPendingSelfIntent(context, W_IN_CLICKED));
+		intent = new Intent(W_OUT_CLICKED);
+		rViews.setOnClickPendingIntent(R.id.btnWOUT,
+				getPendingSelfIntent(context, W_OUT_CLICKED));
 		appWidgetManager.updateAppWidget(watchWidget, rViews);
 	}
 
@@ -45,6 +51,10 @@ public class MyWidgetProvider extends AppWidgetProvider {
 		// TODO Auto-generated method stub
 		super.onReceive(context, intent);
 		if (intent.getAction().equals(W_IN_CLICKED)) {
+			// IN stamp
+			Toast.makeText(context, "Premuto IN!", Toast.LENGTH_SHORT).show();
+		} else if (intent.getAction().equals(W_IN_CLICKED)) {
+			// OUT stamp
 			Toast.makeText(context, "Premuto IN!", Toast.LENGTH_SHORT).show();
 		}
 		// else if
@@ -55,11 +65,11 @@ public class MyWidgetProvider extends AppWidgetProvider {
 
 		return true;
 	}
-	
+
 	protected PendingIntent getPendingSelfIntent(Context context, String action) {
-	    Intent intent = new Intent(context, getClass());
-	    intent.setAction(action);
-	    return PendingIntent.getBroadcast(context, 0, intent, 0);
+		Intent intent = new Intent(context, getClass());
+		intent.setAction(action);
+		return PendingIntent.getBroadcast(context, 0, intent, 0);
 	}
 
 }
